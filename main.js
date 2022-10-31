eaf30aa85300c64ae9d0c91409c6de0cbca4db7c
PisOliver <pisoliver@icloud.com>
2022-10-31 10:24:22 +0100

    Placed events to different folder, and simplified main code

--- main.js
+++ main.js
@@ -14,9 +14,11 @@ const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 //Loading the commands in
 client.commands = new Collection();
 
+//Filtering the command files
 const commandsPath = path.join(__dirname, 'commands');
 const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 
+// Going through the command files
 for (const file of commandFiles) {
 	const filePath = path.join(commandsPath, file);
 	const command = require(filePath);
@@ -28,34 +30,19 @@ for (const file of commandFiles) {
 	}
 }
 
-// When the client is ready, run this code (only once)
-// We use 'c' for the event parameter to keep it separate from the already defined 'client'
-client.once(Events.ClientReady, c => {
-	console.log(`Ready! Logged in as ${c.user.tag}`);
-});
+//Loading the events
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
 
-client.on(Events.InteractionCreate, async interaction => {
-    //Required if the text entered is not a command, return nothing
-	if (!interaction.isChatInputCommand()) return;
-
-    const command = interaction.client.commands.get(interaction.commandName);
-
-    //If the entered command was not found
-	if (!command) {
-		console.error(`No command matching ${interaction.commandName} was found.`);
-		return;
+for (const file of eventFiles) {
+	const filePath = path.join(eventsPath, file);
+	const event = require(filePath);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
 	}
-
-    //Run the entered command, or return an error if the run was not successful
-	try {
-		await command.execute(interaction);
-	} catch (error) {
-		console.error(error);
-		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-	}
-
-	console.log(interaction);
-});
+}
 
 // Log in to Discord with your client's token
 client.login(token);
