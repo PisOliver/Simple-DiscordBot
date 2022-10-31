78b625eb2b2734c36cb93d1fd8fdbe41b469cfb3
PisOliver <pisoliver@icloud.com>
2022-10-31 10:53:18 +0100

    Started implementing new commands

---
+++ commands/echo.js
@@ -0,0 +1,21 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('echo')
+        .setDescription('Replies with the text you entered')
+        .addStringOption(option => 
+            option.setName('input')
+                .setDescription('The input to echo back')
+                .setRequired(true))
+        .addChannelOption(option =>
+            option.setName('channel')
+                .setDescription('The channel to echo into'))
+        .addBooleanOption(option => 
+            option.setName('emphemeral')
+                .setDescription('Whether the echo should be emphemeral')),
+    async execute(interaction) {
+        const text = interaction.options.getString('input')
+        await interaction.reply(text)
+    },
+};
